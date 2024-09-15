1a6cabb057fe4992f34e73890e83e1e153ce7d3e
Suhail Shaikh <suhail.shaikh.ext@siemens.com>
2024-09-16 00:37:27 +0530

    first commit

---
+++ src/webparts/usersPermissions/components/IUsersPermissionsProps.ts
@@ -0,0 +1,7 @@
+export interface IUsersPermissionsProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/usersPermissions/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IUsersPermissionsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'UsersPermissionsWebPartStrings' {
+  const strings: IUsersPermissionsWebPartStrings;
+  export = strings;
+}
